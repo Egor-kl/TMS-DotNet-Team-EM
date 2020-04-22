d44eec18797a10447ba9a2d6325fccdebc993a67
Straltsou Maxim <green1971weekend1@gmail.com>
2020-04-22 22:26:10 +0300

    feat: API requests for getting currency data,currency model

---
+++ src/ExchangeCurrencyProject/ExchangeCurrencyDisplay/Model/Rates.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace NbrbAPI.Models
+{
+    /// <summary>
+    /// Данные курса
+    /// </summary>
+    public class Rate
+    {
+        /// <summary>
+        /// ID Курса
+        /// </summary>
+        [Key]
+        public int Cur_ID { get; set; }
+        /// <summary>
+        /// Время обновления курса
+        /// </summary>
+        public DateTime Date { get; set; }
+        /// <summary>
+        /// Аббревиатура
+        /// </summary>
+        public string Cur_Abbreviation { get; set; }
+        /// <summary>
+        /// Количество условных едениц
+        /// </summary>
+        public int Cur_Scale { get; set; }
+        /// <summary>
+        /// Название валюты
+        /// </summary>
+        public string Cur_Name { get; set; }
+        /// <summary>
+        /// Оффициальный курс
+        /// </summary>
+        public decimal? Cur_OfficialRate { get; set; }
+    }
+    /// <summary>
+    /// Сокращенные данные курса
+    /// </summary>
+    public class RateShort
+    {
+        /// <summary>
+        /// ID Курса
+        /// </summary>
+        public int Cur_ID { get; set; }
+        /// <summary>
+        /// Время обновления курса
+        /// </summary>
+        [Key]
+        public System.DateTime Date { get; set; }
+        /// <summary>
+        /// Оффициальный курс
+        /// </summary>
+        public decimal? Cur_OfficialRate { get; set; }
+    }
+
+}
---
+++ src/ExchangeCurrencyProject/ExchangeCurrencyDisplay/Program.cs
@@ -0,0 +1,108 @@
+﻿using NbrbAPI.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ExchangeCurrencyDisplay
+{
+    class Program
+    {
+        static readonly HttpClient client = new HttpClient();
+        static bool alive = true;
+        static int input;
+        static async Task Main()
+        {
+            Console.WriteLine("Вас приветствует справочник валют\n");
+            while(alive)
+            {
+                try
+                {
+                    Console.WriteLine("Введите цифрой желаемое действие\n");
+                    Console.WriteLine("1.Посмотреть курс 2.Получить список доступных валют 3.Выход");
+                    input = Convert.ToInt32(Console.ReadLine());
+
+                    switch (input)
+                    {
+                        case 1:
+                            await GetCurrencyRateInfo();
+                            break;
+                        case 2:
+                            await GetCurrencyAsync();
+                            break;
+                        case 3:
+                            alive = false;
+                            break;
+                        default:
+                            Console.WriteLine("Неккоректное значение");
+                            break;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+        /// <summary>
+        /// Интерфейс получения данных о курсах
+        /// </summary>
+        /// <returns></returns>
+        static async Task GetCurrencyRateInfo()
+        {
+            try
+            {
+                Console.WriteLine("Введите ID желаемого курса, информацию которого хотите отобразить");
+                Console.WriteLine("Популярные курсы:\n");
+                Console.WriteLine("Доллар США ID: 145");
+                Console.WriteLine("Eвро ID: 292");
+                Console.WriteLine("Российский рубль ID:298");
+
+                input = Convert.ToInt32(Console.ReadLine());
+
+                await GetRateAsync(input);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        /// <summary>
+        /// Получить весь список доступных валют в API
+        /// </summary>
+        public static async Task GetCurrencyAsync()
+        {
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync($"https://www.nbrb.by/api/exrates/currencies");
+                string responseMessage = await response.Content.ReadAsStringAsync();
+                response.EnsureSuccessStatusCode();
+
+                var currency_list = JsonConvert.DeserializeObject<Currency[]>(responseMessage);
+                foreach(Currency cur in currency_list)
+                {
+                    Console.WriteLine($"{cur.Cur_Name} ID: {cur.Cur_ID}");
+                }
+            }
+            catch(HttpRequestException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }  
+        }
+        /// <summary>
+        /// Получить данные о конкретном курсе
+        /// </summary>
+        /// <param name="cur_id">ID курса</param>
+        public static async Task GetRateAsync(int cur_id)
+        {
+            HttpResponseMessage response = await client.GetAsync($"https://www.nbrb.by/api/exrates/rates/ {cur_id}");
+            string responseMessage = await response.Content.ReadAsStringAsync();
+            response.EnsureSuccessStatusCode();
+
+            Rate currency = JsonConvert.DeserializeObject<Rate>(responseMessage);
+            Console.WriteLine($"{currency.Cur_Name} \t Количество у.е - {currency.Cur_Scale} \t Курс по НБРБ - {currency.Cur_OfficialRate} \t Дата обновления - {currency.Date.ToLongDateString()}");
+        }
+    }
+}
